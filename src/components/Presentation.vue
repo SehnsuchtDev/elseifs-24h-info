9dde105bdddd7a7bcbd14a6ccd8192e9c69cc6d0
kuani50 <contact@endocraft.fr>
2025-05-24 00:45:04 +0200

    Refactor components and styles: Add Presentation component, update HelloWorld button style, and adjust global styles

---
+++ src/components/Presentation.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div class="h-screen flex flex-col justify-center">
+        <h1>Les Frères Lumière : Pionniers du Cinéma</h1>
+        <article class="mt-[5vh] flex justify-center align-middle gap-[2vw] px-[10vw]">
+            <img class="rounded-xl  max-w-full max-h-full object-contain" style="width: 40vw; height: 50vh;"  src="../assets/Fratelli_Lumiere.jpg" alt="Les frères lumière">
+            <div class="content">
+                <h2>Qui sont les frères Lumière ?</h2>
+                <p>Les frères Lumière, Auguste et Louis, sont souvent considérés comme les pères du cinéma. Nés à Besançon, en France, au 19ème siècle, ils ont marqué l'histoire avec leur invention révolutionnaire : le cinématographe.</p>
+                <h2>Leur Invention</h2>
+                <p>En 1895, les frères Lumière ont présenté leur premier film, "La Sortie de l'Usine Lumière à Lyon", marquant ainsi la naissance du cinéma. Leur invention, le cinématographe, permettait non seulement de projeter des films, mais aussi de les enregistrer, ce qui était une avancée majeure pour l'époque.</p>
+                <h2>Leur Héritage</h2>
+                <p>Les frères Lumière ont ouvert la voie à une nouvelle forme d'art et de divertissement. Leur travail a inspiré des générations de cinéastes et a jeté les bases de l'industrie cinématographique moderne. Aujourd'hui, leur héritage est célébré dans le monde entier, et leur nom reste synonyme d'innovation et de créativité.</p>
+                <h2>Une Visionnaire Époque</h2>
+                <p>À une époque où la technologie était en pleine évolution, les frères Lumière ont su voir le potentiel du mouvement capturé sur pellicule. Leur vision a transformé la manière dont nous percevons et interagissons avec le monde, faisant d'eux des figures incontournables de l'histoire du cinéma.</p>
+            </div>
+        </article>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
