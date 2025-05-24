2bf1e0607d0a429964e3d6b0329d4b74e677474d
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-05-24 03:45:00 +0200

    spetacle

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import BidCard from './components/BidCard.vue'
 import Presentation from './components/Presentation.vue';
+import VideoPre from './components/VideoPre.vue'
 </script>
 
 <template>
     <Presentation />
+    <VideoPre />
     <BidCard />
 </template>
 
